2711541f1aa57bd85f393a0aa7ff2ecf0dd40673
AntiusDragon <Braizytojas@hotmail.com>
2023-09-19 22:38:59 +0300

    Numbers

---
+++ kintamieji/index.js
@@ -0,0 +1,9 @@
+/*
+KINTAMIEJI
+
+- number
+- string (tekstas)
+- boolean (true / false)
+- array (masyvas / kolekcija/ sąrašas / listas/ matrica /arėkjas)
+- object (objektas - kompleksinei informacijai aprasyti)
+*/
---
+++ kintamieji/number.js
@@ -0,0 +1,28 @@
+/*
+NUMBER
+
+- teigiami / neigiami
+- sveikieji / desimtainiai
+- normalus / nenormalus
+
+Iniciavimo budas:
+- const (default)
+- let (jei nkinta informacija)
+- var (niekada 👀)
+*/
+
+
+const ammzius = 99;
+let pinigine = 0;
+var minusas = -7.56;
+
+console.log(ammzius);
+console.log(pinigine);
+console.log(minusas);
+
+
+const x1 = 1;
+const x2 = 2;
+const x3 = 3;
+const x4 = 4;
+const x5 = 5;
